66236c697fbf4e3a7f2036751802fe0b32de18ee
Ryan <ryanlim0616@gmail.com>
2016-12-22 15:11:04 +0800

    Remove PreCompile Header File

--- PrintJobManager/PrintJobManager/PrintJobManager.cpp
+++ PrintJobManager/PrintJobManager/PrintJobManager.cpp
@@ -1,11 +1,11 @@
-// PrintJobManager.cpp : Defines the entry point for the console application.
-//
 
-#include "stdafx.h"
+#include <iostream>
+using namespace std;
 
 
 int main()
 {
+	cout << "gege";
     return 0;
 }
 
